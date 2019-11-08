936ec9e134dd81261b25e04e3e2a838b3b340a6c
Will Quanstrom <wquanstr215@gmail.com>
2019-11-08 11:38:50 -0800

    add new property to pizza object and update prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,24 @@
 // back end logic
-var Pizza = function(size, toppings){
-  this.size = size'
-  this.toppings = toppings
+var Pizza = function(size, veggies, meats){
+  this.size = size,
+  this.veggies = veggies,
+  this.meats = meats
 }
 
 Pizza.prototype.price = function(){
   var price = 0;
+  var small;
+  var medium;
+  var large;
   if(this.size == small){
     price += 10;
   } else if(this.size == medium){
     price += 15;
   } else if(this.size == large){
     price += 20;
-  } else if(this.toppings == vegetable){
+  } else if(this.veggies == vegetable){
     price += 3;
-  } else if(this.toppings == meat){
+  } else if(this.meats == meat){
     price += 5;
   }
   return price;
@@ -22,6 +26,17 @@ Pizza.prototype.price = function(){
 
 
 
-
-
 // front end logic
+$(document).ready(function(){
+  $("#toSubmit").submit(function(event){
+    event.preventDefault();
+    var inputSize = $("#size").val();
+    var inputVeggies = $("#veggies").val();
+    var inputMeat = $("#meat").val();
+
+  var order = new Pizza(inputSize, inputVeggies, inputMeat);
+  console.log(order);
+  $("#output").text(order.price());
+
+  });
+});
